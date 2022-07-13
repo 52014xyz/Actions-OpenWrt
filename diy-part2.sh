030c1c390d174114b98dd2714056337ad67a4753
缘空空 <32029539+NaiHeKK@users.noreply.github.com>
2022-07-13 16:25:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# 修改机器名称
+sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
+
+# Modify zzz-default-settings
+export orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+export date_version=$(date +"%Y-%m-%d")
+sed -i "s/${orig_version}/${orig_version} (${date_version})/g" package/lean/default-settings/files/zzz-default-settings
